30efb6137098b4079426d25686045aa375f31b75
Joni Weiss <hi@joniwebgirl.com>
2015-11-30 11:15:32 -0700

    updated menu

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,22 @@
 <!-- Begin CSS Vertical Menu - menu ver 3.1 2009
 
 
-var leftspacing		= "20"		
-var rightspacing	= "20"		
-var menuside		= "right"	
+var leftspacing		= "20"
+var rightspacing	= "20"
+var menuside		= "right"
 var menuwidth		= "400"
 
 document.write('<table cellpadding="0" cellspacing="0" border="0" class="menutable"><tr><td class="printhide" align="'+menuside+'">');
 document.write('<table cellpadding="0" cellspacing="0" border="0"><tr><td width="'+leftspacing+'">');
-document.write('<img src="img/spacer.gif" width="'+leftspacing+'" height="1"><br>');
+document.write('<img src="img/spacer.gif" width="'+leftspacing+'" height="1.25"><br>');
 document.write('</td><td>');
 
 document.write('<ul id="menunav">');
 
-document.write('  <li style="width: 95px;"><a href="index.html">home&#160;&#160;&#160;&#160;</a>'); 
+document.write('  <li style="width: 95px;"><a href="index.html">home&#160;&#160;&#160;&#160;</a>');
 document.write('  </li>');
 
-document.write('  <li style="width: 100px;"><a href="services.htm">services&#160;&#160;&#160;&#160;</a>'); 
+document.write('  <li style="width: 100px;"><a href="services.htm">services&#160;&#160;&#160;&#160;</a>');
 document.write('    <ul>');
 document.write('      <li><a href="services_whydogmode.htm">why dogmode</a></li>');
 document.write('      <li><a href="services_boarding.htm">boarding</a></li>');
@@ -66,10 +66,10 @@ document.write('  <li style="width: 95px;"><a href="contact.htm">contact&#160;&#
 document.write('</ul>');
 
 document.write('</td><td width="'+rightspacing+'">');
-document.write('<img src="img/spacer.gif" width="'+rightspacing+'" height="1"><br>');
+document.write('<img src="img/spacer.gif" width="'+rightspacing+'" height="1.25"><br>');
 document.write('</td></tr></table>');
 document.write('</td></tr><tr><td width="'+menuwidth+'">');
-document.write('<img src="img/spacer.gif" width="'+menuwidth+'" height="1"><br>');
+document.write('<img src="img/spacer.gif" width="'+menuwidth+'" height="1.25"><br>');
 document.write('</td></tr></table>');
 
 // END -->
